8db28f36ad1bbe6bcc093782f98182f8d40441bd
feishoufeng <fsf0826@163.com>
2017-03-23 16:26:12 +0800

    工具

---
+++ tools/src/com/my/test/CreateThumbnail.java
@@ -0,0 +1,81 @@
+package com.my.test;
+
+import java.io.FileInputStream;
+import java.io.InputStream;
+
+import org.apache.commons.lang3.StringUtils;
+
+import sun.misc.BASE64Encoder;
+
+public class CreateThumbnail {
+	public static String createThumbnail(String imgFile) {
+		InputStream in = null;
+		byte[] data = null;
+		// byte[] dt = null;
+
+		// 读取图片字节数组
+		try {
+			in = new FileInputStream(imgFile);
+			data = new byte[in.available()];
+			in.read(data);
+			// String str = new String(data, "utf-8");
+			// BASE64Decoder decoder = new BASE64Decoder();
+			// dt = decoder.decodeBuffer(str);
+
+			BASE64Encoder encoder = new BASE64Encoder();
+			String imgStr = encoder.encode(data);
+
+			// BASE64Decoder decoder = new BASE64Decoder();
+			// byte[] b = decoder.decodeBuffer(imgStr);
+			// for (int i = 0; i < b.length; ++i) {
+			// if (b[i] < 0) {// 调整异常数据
+			// b[i] += 256;
+			// }
+			// }
+			//
+			// FileOutputStream outPut = new FileOutputStream("D://11112.jpg");
+			// outPut.write(b);
+			// outPut.flush();
+			// outPut.close();
+			in.close();
+			return imgStr;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return "";
+	}
+
+	public static String transmitMsg(String oldStr) {
+		String newStr = oldStr;
+
+		if (StringUtils.isNotEmpty(oldStr)) {
+			// 格式化所需要的字符串样式
+			String formatStr = oldStr.replaceAll("<img", "<<<img");
+			formatStr = formatStr.replaceAll("\">", "\"><<");
+
+			String[] strs = formatStr.split("<<");
+			for (String str : strs) {
+				if (str.contains("<img")) {
+					String imgIndex = str.substring(str.lastIndexOf("/") + 1, str.indexOf(".gif"));
+
+					int code = 0xa4565;
+					char[] chars = Character.toChars(code);
+					String key = Character.toString(chars[0]);
+					for (int i = 1; i < chars.length; ++i) {
+						key = key + Character.toString(chars[i]);
+					}
+					newStr = newStr.replace(str, key);
+				}
+			}
+			return newStr;
+		} else {
+			return "";
+		}
+	}
+
+	public static void main(String[] args) {
+		String str = "faefqawf<img src=\"/assets/images/Emoticon/4.gif\" style=\"max-width: 24px;\">fawefa<img src=\"/assets/images/Emoticon/35.gif\" style=\"max-width: 24px;\">feawfga";
+		String newStr = transmitMsg(str);
+		System.out.println(newStr);
+	}
+}
---
+++ tools/src/com/my/test/ImgCompress.java
@@ -0,0 +1,103 @@
+package com.my.test;
+
+import java.io.*;
+import java.util.Date;
+import java.awt.*;
+import java.awt.image.*;
+import javax.imageio.ImageIO;
+
+import org.apache.commons.codec.binary.Base64;
+
+/**
+ * 图片压缩处理
+ * 
+ * @author ShoufengFei
+ *
+ */
+public class ImgCompress {
+	private Image img;
+	private int width;
+	private int height;
+
+	@SuppressWarnings("deprecation")
+	public static void main(String[] args) throws Exception {
+		System.out.println("开始：" + new Date().toLocaleString());
+		ImgCompress imgCom = new ImgCompress("D://11111.jpg");
+		String imgStr = imgCom.resizeFix(400, 400);
+		System.out.println(imgStr);
+		System.out.println("结束：" + new Date().toLocaleString());
+	}
+
+	/**
+	 * 构造函数
+	 */
+	public ImgCompress(String fileName) throws IOException {
+		File file = new File(fileName);// 读入文件
+		img = ImageIO.read(file); // 构造Image对象
+		width = img.getWidth(null); // 得到源图宽
+		height = img.getHeight(null); // 得到源图长
+	}
+
+	/**
+	 * 按照宽度还是高度进行压缩
+	 * 
+	 * @param w
+	 *            int 最大宽度
+	 * @param h
+	 *            int 最大高度
+	 */
+	public String resizeFix(int w, int h) throws IOException {
+		if (width / height > w / h) {
+			return resizeByWidth(w);
+		} else {
+			return resizeByHeight(h);
+		}
+	}
+
+	/**
+	 * 以宽度为基准，等比例放缩图片
+	 * 
+	 * @param w
+	 *            int 新宽度
+	 */
+	public String resizeByWidth(int w) throws IOException {
+		int h = (int) (height * w / width);
+		return resize(w, h);
+	}
+
+	/**
+	 * 以高度为基准，等比例缩放图片
+	 * 
+	 * @param h
+	 *            int 新高度
+	 */
+	public String resizeByHeight(int h) throws IOException {
+		int w = (int) (width * h / height);
+		return resize(w, h);
+	}
+
+	/**
+	 * 强制压缩/放大图片到固定的大小
+	 * 
+	 * @param w
+	 *            int 新宽度
+	 * @param h
+	 *            int 新高度
+	 */
+	public String resize(int w, int h) throws IOException {
+		// SCALE_SMOOTH 的缩略算法 生成缩略图片的平滑度的 优先级比速度高 生成的图片质量比较好 但速度慢
+		BufferedImage image = new BufferedImage(w, h, BufferedImage.TYPE_INT_RGB);
+		image.getGraphics().drawImage(img, 0, 0, w, h, null); // 绘制缩小后的图
+		// File destFile = new File("D://11112.jpg");
+		// FileOutputStream out = new FileOutputStream(destFile); // 输出到文件流
+		// // 可以正常实现bmp、png、gif转jpg
+		// JPEGImageEncoder encoder = JPEGCodec.createJPEGEncoder(out);
+		// encoder.encode(image); // JPEG编码
+		// out.close();
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
+		ImageIO.write(image, "jpg", out);
+		byte[] b = out.toByteArray();
+		String imgStr = Base64.encodeBase64String(b);
+		return imgStr;
+	}
+}
